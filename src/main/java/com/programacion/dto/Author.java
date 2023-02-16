887ffeb473d9c85f237e3d13173ea28faecbe4a8
erickdp <erick@hotmail.com>
2023-02-16 17:17:05 -0500

    update mapping

--- src/main/java/com/programacion/dto/Author.java
+++ src/main/java/com/programacion/dto/Author.java
@@ -1,9 +1,6 @@
 package com.programacion.dto;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.io.Serializable;
@@ -20,6 +17,8 @@ public class Author implements Serializable {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private long id;
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
 }
